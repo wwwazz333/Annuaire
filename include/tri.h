96ab50ecbe558d0f548698bdae8d753a93377eef
Virgile MÉNARD-BEDIANT <virgile.mEnard-bediant@etu.univ-lyon1.fr>
2021-11-22 10:49:39 +0100

    quick sort fonctionne

--- include/tri.h
+++ include/tri.h
@@ -3,8 +3,27 @@
 
 #include "User.h"
 #include "string.h"
-
+/**
+ * @brief tire le tableau tab de l'indice first à last
+ * 
+ * @pre first et last des index de tab
+ * @post le tableau tab est tiré par le nom
+ * 
+ * @param tab le tableau a trié
+ * @param first où commencer
+ * @param last recommander : len-1
+ */
 void quick_sort(user* tab, int first, int last);
+
+/**
+ * @brief permute user a et b
+ * 
+ * @pre aucune
+ * @post a et b sont permuté
+ *  
+ * @param a 
+ * @param b 
+ */
 void permute(user* a, user* b);
 
 #endif
