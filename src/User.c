859593be9661e046db5754c37593b92ffd788268
Virgile MÉNARD-BEDIANT <virgile.mEnard-bediant@etu.univ-lyon1.fr>
2021-11-24 08:55:09 +0100

    insertion bonne endroite mais ne gère pas si tableau pas assez grand

--- src/User.c
+++ src/User.c
@@ -1,6 +1,6 @@
 #include "User.h"
-#include "tri.h"
 #include "Verif.h"
+#include "tri.h"
 
 user input_user()
 {
@@ -29,22 +29,19 @@ user input_user()
     return u;
 }
 
-
-
-
 int insert_user(user tab[], int taille, user u)
 {
     int index_a_ajouter = recherche_emplacement(tab, taille, u.nom);
-    printf("on ajoute à %d\n", ++index_a_ajouter);
+    printf("on ajoute à %d\n", index_a_ajouter);
 
     permute(&tab[index_a_ajouter], &u);
-    int i = 1;
-    while (i < taille && is_del(u) != 1) {
-        permute(&tab[index_a_ajouter + i], &u);
+    index_a_ajouter++;
+    while (index_a_ajouter < taille && is_del(u) != 1) {
+        permute(&tab[index_a_ajouter], &u);
 
-        i++;
+        index_a_ajouter++;
     }
-    if (is_del(u) != 1 || index_a_ajouter >= taille) {
+    if (!is_del(u) || index_a_ajouter >= taille-1) {
         printf("il n'y avais pas assez de place dans le tableau et on a pas encore prévue\n");
     }
 
@@ -53,23 +50,24 @@ int insert_user(user tab[], int taille, user u)
 
 int recherche_emplacement(user tab[], int taille, char nom[64]) // recherche le dernier
 {
-    int i = -1;
+    int millieu;
     int gauche = 0;
     int droite = taille - 1;
     while (gauche <= droite) {
-        i = (droite + gauche) / 2;
-        if (strcmp((char*)&tab[i].nom, nom) < 0) {
-            gauche = i + 1;
-        } else if (strcmp((char*)&tab[i].nom, nom) > 0) {
-            droite = i - 1;
+        millieu = (droite + gauche) / 2;
+        if (strcmp((char*)&tab[millieu].nom, nom) < 0) {
+            gauche = millieu + 1;
+        } else if (strcmp((char*)&tab[millieu].nom, nom) > 0) {
+            droite = millieu - 1;
         } else {
-            while (strcmp((char*)&tab[i + 1].nom, nom) == 0) {
-                i++;
+            while (strcmp((char*)&tab[millieu + 1].nom, nom) == 0) {
+                gauche++;
             }
-            return i;
+            return gauche;
         }
     }
-    return i;
+    printf("droite : %d\tgauche : %d\n", droite, gauche);
+    return gauche;
 }
 void usercpy(user* dst, user* src)
 {
